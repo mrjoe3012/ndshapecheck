ea2183f27d89e91c92214266a3105cb01b282a62
Joseph Agrane <josephagrane@gmail.com>
2025-04-23 22:11:01 +0200

    added docstring to shape check

--- src/shapecheck/shape_check.py
+++ src/shapecheck/shape_check.py
@@ -1,10 +1,16 @@
-"""
-"""
 from __future__ import annotations
 from shapecheck.shape_rule import ShapeRule
 
 __all__ = ["ShapeCheck"]
 
 class ShapeCheck:
+    """
+    Contains a context for maintaining consistency of symbols between
+    subsequent shape checks.
+    """
     def __call__(self, shape_str: str) -> ShapeRule:
+        """
+        :param shape_str: The shape string to parse.
+        :returns: a shape rule which can be used to check if an array's shape conforms to it.
+        """
         return ShapeRule(self, shape_str)
