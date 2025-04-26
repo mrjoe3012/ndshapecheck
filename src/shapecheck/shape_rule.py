fb09f190b2e21b047c80970732eb5eade733e509
mrjoe3012 <51813263+mrjoe3012@users.noreply.github.com>
2025-04-26 09:49:42 +0100

    feat: #22 add why property

--- src/shapecheck/shape_rule.py
+++ src/shapecheck/shape_rule.py
@@ -13,13 +13,14 @@ __all__ = ["ShapeRule"]
 if TYPE_CHECKING:
     from shapecheck.shape_check import ShapeCheck
 
-def _parse_shape_str(shape_str: str) -> tuple[list[str], list[int | None]]:
+def _parse_shape_str(shape_str: str) -> tuple[list[str], list[Optional[str]], list[int | None]]:
     """
     :param shape_str: A shape string consisting of symbols and literals seperated
     by commas.
-    :returns: Tuple containing the symbols and literals.
+    :returns: Tuple containing the symbols, modifiers and literals.
     """
     symbols: list[str] = []
+    modifiers: list[Optional[str]] = []
     literals: list[Optional[int]] = []
     
     elem_re = r'[a-zA-Z0-9_]+[+*?]?'
@@ -31,7 +32,7 @@ def _parse_shape_str(shape_str: str) -> tuple[list[str], list[int | None]]:
     for elem in elements:
         num = None
         try:
-            if elem[-1] in '?*+':
+            if elem[-1] in tuple('?*+'):
                 num = int(elem[:-1])
             else:
                 num = int(elem)
@@ -39,22 +40,26 @@ def _parse_shape_str(shape_str: str) -> tuple[list[str], list[int | None]]:
             num = None
         symbols.append(elem)
         literals.append(num)
-    return symbols, literals
+        if elem[-1] in tuple('?*+'):
+            modifiers.append(elem[-1])
+        else:
+            modifiers.append(None)
+    return symbols, modifiers, literals
 
-def _construct_rule_regex(symbols: list[str], literals: Sequence[int | str | None]) -> str:
+def _construct_rule_regex(symbols: list[str], modifiers: list[Optional[str]],
+                          literals: Sequence[int | str | None]) -> str:
     """
     :param symbols: Symbols from __parse_shape_str
+    :param modifiers: The modifiers which are one of * ? + or None.
     :param literals: Literal values from __parse_shape_str
     :returns: The regex pattern validating the rule.
     """
     n = len(symbols) # == len(literals)
     regex_parts: list[str] = []
     for i in range(n):
-        modifier = ''
-        if symbols[i][-1] in '*+':
-            modifier = f'{symbols[i][-1]}?'
-        elif symbols[i][-1] == '?':
-            modifier = symbols[i][-1]
+        modifier: str = modifiers[i] or ''
+        if modifier in ['*', '+']:
+            modifier += '?'
         if literals[i] is not None:
             element = str(literals[i])
         else:
@@ -75,7 +80,7 @@ class ShapeRule:
         """
         self._context = context
         self._shape_str = shape_str
-        self._symbols, self._literals = _parse_shape_str(self._shape_str)
+        self._symbols, self._modifiers, self._literals = _parse_shape_str(self._shape_str)
 
     def __get_pattern(self) -> str:
         """
@@ -84,10 +89,12 @@ class ShapeRule:
         :returns: Regex that can be used to match shape strings.
         """
         literals: list[str | int | None] = list(self._literals)
+        modifiers: list[Optional[str]] = list(self._modifiers)
         for i, symbol in enumerate(self._symbols):
             if symbol in self._context._vars:
                 literals[i] = ','.join(map(str, self._context._vars[symbol]))
-        return _construct_rule_regex(self._symbols, literals)
+                modifiers[i] = None
+        return _construct_rule_regex(self._symbols, modifiers, literals)
 
     @overload
     def check(self, shape: HasShape) -> bool: ...
@@ -110,14 +117,17 @@ class ShapeRule:
         pattern = self.__get_pattern()
         match = re.fullmatch(pattern, shape_str)
         if match is None:
+            # construct shape string with context values added in
+            sub_shape_str = ','.join(
+                [symbol + (f'={self._context._vars[symbol]}'
+                    if symbol in self._context._vars else '') for symbol in self._symbols]
+            )
+            self._context._why = f"The shape {shape} does not match the rule '{sub_shape_str}'."
             return False
         for i, group_str in enumerate(match.groups()):
             # dont' enforce consistency for literals
             if self._literals[i] is not None:
                 continue
             group_tuple = tuple(map(int, filter(bool, group_str.split(','))))
-            prev_tuple = self._context._vars.get(self._symbols[i], group_tuple)
-            if group_tuple != prev_tuple:
-                return False
             self._context._vars[self._symbols[i]] = group_tuple
         return True
